ca6736691127e1b3510cf1bf3de7251dee91951e
asooge <asooge@gmail.com>
2019-09-25 12:19:17 -0400

    Wrote curl scripts for user Authorization
    wrote curl scripts for sign-up, sign-in, sign-out, and change
    password. Tested and working.

---
+++ curl-scripts/sign-in.sh
@@ -0,0 +1,8 @@
+curl --include --request POST "https://tic-tac-toe-wdi.herokuapp.com/sign-in"\
+  --header "Content-Type: application/json" \
+  --data '{
+  "credentials": {
+    "email": "'"${EMAIL}"'",
+    "password": "'"${PASSWORD}"'"
+  }
+}'
