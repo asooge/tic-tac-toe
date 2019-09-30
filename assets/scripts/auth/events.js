6d37db75370dd0516f4810b4f6cb06fbca43eb52
asooge <asooge@gmail.com>
2019-09-30 09:16:28 -0400

    Updated user auth submit button
    Added conditionals that the submit button will execute
    only when the required fields are complete.

--- assets/scripts/auth/events.js
+++ assets/scripts/auth/events.js
@@ -52,20 +52,20 @@ const onSubmit = function (event) {
   const form = event.target
   const formData = getFormFields(form)
   console.log(formData)
-  if (display.status.signUp) {
+  if (display.status.signUp && $('#email').val() && $('#password').val() && $('#password-confirm').val()) {
     console.log("on submit sign up")
     delete formData.passwords
     api.signUp(formData)
       .then(ui.onSignUpSuccess)
       .catch(ui.onSignUpFailure)
-  } else if (display.status.signIn) {
+  } else if (display.status.signIn && $('#email').val() && $('#password').val()) {
     console.log("on submit sign in")
     delete formData.credentials.password_confirmation
     console.log(formData)
     api.signIn(formData)
       .then(ui.onSignInSuccess)
       .catch(ui.onSignInFailure)
-  } else if (display.status.changePass) {
+  } else if (display.status.changePass && $('#old-pass').val() && $('#new-pass').val()) {
     console.log("changePass working")
     delete formData.credentials
     console.log(formData)
